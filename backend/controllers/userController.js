1b928dec1ec1d7357e15497a0d482ba4ba927a25
Shubham Gupta <ashutosh5118@gmail.com>
2023-10-17 08:28:56 +0530

    Login and Create Jwt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,6 +3,8 @@ const asyncHandler=require('express-async-handler')
 const bcrypt=require('bcryptjs')
 const User=require('../models/userModel')
 
+const jwt=require('jsonwebtoken')
+
 // @desc Register a new user
 // @route /api/users
 // @access Public
@@ -39,14 +41,14 @@ const registerUser=asyncHandler(async(req,res)=>{
         res.status(201).json({
             _id:user._id,
             name:user.name,
-            email:user.email
+            email:user.email,
+            token:generateToken(user._id)
         })
     }else{
         res.status(400)
         throw new error('Invalid user data')
     }
 
-    
 
 
 })
@@ -55,10 +57,41 @@ const registerUser=asyncHandler(async(req,res)=>{
 // @route /api/users/login
 // @access Public
 const loginUser=asyncHandler(async(req,res)=>{
-    res.send('Login Route')
+    const {email,password}=req.body;
+
+    const user=await User.findOne({email});
+
+    if(user && (await bcrypt.compare(password,user.password))){
+        res.status(200).json({
+            _id:user._id,
+            name:user.name,
+            email:user.email,
+            token:generateToken(user._id)
+        })
+      } else {
+        res.status(401)
+        throw new Error('Invalid credentials')
+      }
+
 })
+
+
+// @desc Register a new user
+// @route /api/users
+// @access Public
+const getMe=asyncHandler(async(req,res)=>{
+    res.send('me');
+})
+
+// Generate token
+const generateToken=(id)=>{
+    return jwt.sign({id},process.env.JWT_SECRET,{
+        expiresIn:'30d'
+    })
+}
 
 module.exports={
     registerUser,
     loginUser,
+    getMe
 }
