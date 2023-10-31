ffcdfd7e14ad64a44fcdc6f49bee3007b4be0fe4
Shubham Gupta <ashutosh5118@gmail.com>
2023-10-31 11:50:52 +0530

    components and pages folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
-
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Home from "./pages/Home"
+import {BrowserRouter as Router,Route,Routes} from "react-router-dom";
 
 function App() {
   return (
-    <div >
-       <h1>My-App</h1>
-    </div>
+    <>
+    <Router>
+    <div className="container" >
+       
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/register" element={<Register/>}/>
+          <Route path="/login" element={<Login/>}/>
+        </Routes>
+        </div>
+
+       </Router>
+    </>
+    
+   
   );
 }
 
